c55557b96935fa30ef019d61ffd09e3b32f7dd89
DeLayne LaBove <delayne.labove@gmail.com>
2020-10-10 16:38:35 -0500

    first array

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
     function greet($name, $color) {
      echo "<p>Hi, my $name is blank and my favorite color is $color.</p>";
     }
@@ -8,5 +8,13 @@ greet("Jane", "pink");
 ?>
 
 <h1><?php bloginfo(); ?></h1>
-<p><?php bloginfo('description'); ?></p>
+<p><?php bloginfo('description'); ?></p> -->
+
+<?php
+
+    $names = array('DeLayne', 'Grayson', 'Tuck', 'Mia');
+
+?>
+
+<p>Hello, <?php echo $names[2]; ?>!</p>
 
