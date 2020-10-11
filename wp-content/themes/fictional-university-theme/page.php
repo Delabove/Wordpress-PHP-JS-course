d0a95b65c5ea2a8f15dddafa796ee267c8db1829
DeLayne LaBove <delayne.labove@gmail.com>
2020-10-11 08:36:09 -0500

    changed content to match the curriculum

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -7,7 +7,7 @@
     <div class="page-banner__content container container--narrow">
       <h1 class="page-banner__title"><?php echo the_title(); ?></h1>
       <div class="page-banner__intro">
-        <p>Learn how the school of your dreams got started.</p>
+        <p>DONT FORGET TO CHANGE LATER</p>
       </div>
     </div>
   </div>
